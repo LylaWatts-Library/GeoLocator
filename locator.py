96acdc47ee4bfcaa71a7df985eaf47768ec19f9d
Portgas D Ace <72243953+afnan007a@users.noreply.github.com>
2021-10-27 15:14:04 +0530

    Update locator.py

--- locator.py
+++ locator.py
@@ -1,8 +1,8 @@
 import geocoder, folium
 
-g = geocoder.ip("103.85.207.139")
+g = geocoder.ip("me")
 
 myAddress = g.latlng
 my_map1 = folium.Map(location=myAddress, zoom_start=15)
 
-my_map1.save("my_map.html")
+my_map1.save("my_ip_location.html")
